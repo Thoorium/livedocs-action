a21593d025dfd96d4e334fe8a0e1cecf132b49a7
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2021-04-17 16:57:17 -0400

    Update all href and src.

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,7 @@ cp -R "/LiveDocs/wwwroot/." "${GITHUB_WORKSPACE}/${INPUT_DOCUMENTATION_FOLDER}/.
 
 echo "Updating application path."
 
-sed -i "s/base href=\"\/\"/base href=\"\/"${APPLICATION_NAME}"\/\"/g" "${GITHUB_WORKSPACE}/${INPUT_DOCUMENTATION_FOLDER}/index.html"
+sed -i "s/href=\"/href=\""${APPLICATION_NAME}"\//g" "${GITHUB_WORKSPACE}/${INPUT_DOCUMENTATION_FOLDER}/index.html"
+sed -i "s/src=\"/src=\""${APPLICATION_NAME}"\//g" "${GITHUB_WORKSPACE}/${INPUT_DOCUMENTATION_FOLDER}/index.html"
 
 echo "Done."
