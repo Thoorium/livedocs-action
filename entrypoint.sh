42f26ab18819edd624474c0eccd97e465ad0d906
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2021-04-17 15:50:01 -0400

    Another method to get the repository name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Starting the LiveDocs Generator action."
@@ -7,9 +7,7 @@ if [ -n "$INPUT_DOCUMENTATION_FOLDER" ]; then
   echo "Documentation folder is '$GITHUB_WORKSPACE/$INPUT_DOCUMENTATION_FOLDER'."
 fi 
 
-IN="$GITHUB_REPOSITORY"
-arrIN=(${IN//\// })
-APPLICATION_NAME = ${arrIN[1]}
+APPLICATION_NAME = ${GITHUB_REPOSITORY#*/}
 
 if [ -n "$INPUT_APPLICATION_NAME" ]; then
   APPLICATION_NAME = "$INPUT_APPLICATION_NAME"
